1455a4de87a6bb11f14f10b474a66a9f39361026
Olixhd <132328258+Olixhd@users.noreply.github.com>
2025-11-09 14:47:04 +0100

    added medicine take date update function

--- src/medicine/medicine.py
+++ src/medicine/medicine.py
@@ -1,11 +1,25 @@
 import json
-
+from datetime import date, timedelta
 class Medicine():
     def __init__(self):
         self.medicine = self.read_medicine()
 
-    def read_medicine():
-        with open("src/plants/my_plants.json", 'r') as f:
-            plant_data = json.load(f)
+    def read_medicine(self):
+        with open("src/data/my_medicine.json", 'r') as f:
+            self.medicine = json.load(f)
             
-        return plant_data
+        return self.medicine
+    
+    def update_medicine(self):
+        self.medicine = self.read_medicine()
+        today = date.today()
+        for medicine in self.medicine:
+            take_date = date.fromisoformat(medicine["take_date"])
+            if take_date < today:
+                new_take_date = today + timedelta(days=(medicine["how_often"] - 1)) # minus one because it updates one day later
+                medicine["take_date"] = new_take_date.isoformat()
+        with open("src/data/my_medicine.json", 'w') as f:
+            json.dump(self.medicine, f, ensure_ascii=False, indent=2)
+
+test = Medicine()
+test.update_medicine()
