91b2a0a1bd8e66025d2501d3cc706ee75187a1b9
Olixhd <132328258+Olixhd@users.noreply.github.com>
2025-11-11 21:24:47 +0100

    added different fonts

--- src/display/display.py
+++ src/display/display.py
@@ -2,7 +2,7 @@
 import sys
 sys.path.append('/home/oliver/Documents/ePaper/src')
 from waveshare_epd import epd7in5b_V2
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont  
 import time
 import atexit
 from data.data import Data
@@ -12,6 +12,9 @@ class Display:
     def __init__(self,data):
         self.epd = epd7in5b_V2.EPD()
         self.data:Data = data
+        self.font_large = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24)
+        self.font_medium = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+        self.font_small = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
 
     def demo(self):
         try:
@@ -22,12 +25,12 @@ class Display:
             image_red = Image.new("1", (800, 480), 255)
             draw_red = ImageDraw.Draw(image_red)
             for i, medicine in enumerate(self.data.medicine.today_medicine):
-                draw_black.text((i * 200, 0), medicine["name"]+ " Quantity:" + str(medicine["quantity"]), fill=0)
+                draw_black.text((i * 200, 0), medicine["name"]+ " Quantity:" + str(medicine["quantity"]),font=self.font_large, fill=0)
             
             for i, plant in enumerate(self.data.plants.plants_water_today):
-                draw_black.text((i * 100, 100), plant["name"], fill=0)
+                draw_black.text((i * 100, 100), plant["name"],self.font_large, fill=0)
 
-            draw_black.text((0, 200),"Weather: " + self.data.weather.weather["main"]+ " Temperature: " + str(self.data.weather.weather["feels_like"]), fill=0)
+            draw_black.text((0, 200),"Weather: " + self.data.weather.weather["main"]+ " Temperature: " + str(self.data.weather.weather["feels_like"]),self.font_large, fill=0)
             self.epd.display(self.epd.getbuffer(image_black), self.epd.getbuffer(image_red))
             self.epd.sleep()
 
