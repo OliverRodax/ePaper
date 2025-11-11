52cb3b67b4a101ab8e1943e10e7a69268fcff45d
Olixhd <132328258+Olixhd@users.noreply.github.com>
2025-11-11 21:24:47 +0100

    added different fonts

--- src/display/display.py
+++ src/display/display.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 import sys
-sys.path.append('/home/oliver/Documents/ePaper/src')
+
+sys.path.append("/home/oliver/Documents/ePaper/src")
 from waveshare_epd import epd7in5b_V2
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import time
 import atexit
 from data.data import Data
 
 
 class Display:
-    def __init__(self,data):
+    def __init__(self, data):
         self.epd = epd7in5b_V2.EPD()
-        self.data:Data = data
+        self.data: Data = data
+        self.font_large = ImageFont.truetype(
+            "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24
+        )
+        self.font_medium = ImageFont.truetype(
+            "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18
+        )
+        self.font_small = ImageFont.truetype(
+            "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14
+        )
 
     def demo(self):
         try:
@@ -21,14 +31,34 @@ class Display:
             draw_black = ImageDraw.Draw(image_black)
             image_red = Image.new("1", (800, 480), 255)
             draw_red = ImageDraw.Draw(image_red)
-            for i, medicine in enumerate(self.data.medicine.today_medicine):
-                draw_black.text((i * 200, 0), medicine["name"]+ " Quantity:" + str(medicine["quantity"]), fill=0)
-            
-            for i, plant in enumerate(self.data.plants.plants_water_today):
-                draw_black.text((i * 100, 100), plant["name"], fill=0)
 
-            draw_black.text((0, 200),"Weather: " + self.data.weather.weather["main"]+ " Temperature: " + str(self.data.weather.weather["feels_like"]), fill=0)
-            self.epd.display(self.epd.getbuffer(image_black), self.epd.getbuffer(image_red))
+            for i, medicine in enumerate(self.data.medicine.today_medicine):
+                draw_black.text(
+                    (i * 200, 0),
+                    medicine["name"] + " Quantity:" + str(medicine["quantity"]),
+                    font=self.font_large,
+                    fill=0,
+                )
+
+            for i, plant in enumerate(self.data.plants.plants_water_today):
+                draw_black.text(
+                    (i * 100, 100), plant["name"], font=self.font_large,
+                    fill=0
+                )
+
+            draw_black.text(
+                (0, 200),
+                "Weather: "
+                + self.data.weather.weather["main"]
+                + " Temperature: "
+                + str(self.data.weather.weather["feels_like"]),
+                font=self.font_large,
+                fill=0,
+            )
+
+            self.epd.display(
+                self.epd.getbuffer(image_black), self.epd.getbuffer(image_red)
+            )
             self.epd.sleep()
 
         except Exception as e:
